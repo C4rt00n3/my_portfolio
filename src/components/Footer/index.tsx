1a802171bb3aa23e8c940fb07d372f2d2674d4ac
C4rt00n3 <rafaelcruzpaes@gmail.com>
2023-05-22 17:37:21 -0300

    feat:deploy

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,9 +3,8 @@ import { Text } from "@/styles/Text";
 import { Footer as FooterWrapper } from "./style";
 import { UserImage } from "@/pages/home/style";
 import { userData } from "@/utils/userData";
-import { FaLinkedinIn, FaInstagram, FaFacebookF } from "react-icons/fa";
+import { FaLinkedinIn, FaInstagram } from "react-icons/fa";
 import { Button } from "@/styles/Buttons";
-// import { HandEffect } from "../HandEffect";
 
 export const Footer = (): JSX.Element => {
   return (
